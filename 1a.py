42fe224c19eacca3f11a48199b182217acd7deb5
YehonatanHen <yonatan.h333@gmail.com>
2021-04-08 11:27:20 +0300

    1a fixed according to new instructions

--- 1a.py
+++ 1a.py
@@ -1,25 +1,3 @@
-# function
-# extended_gcd(a, b)
-# (old_r, r) := (a, b)
-# (old_s, s) := (1, 0)
-# (old_t, t) := (0, 1)
-#
-# while r ≠ 0 do
-# quotient := old_r
-# div
-# r
-# (old_r, r) := (r, old_r − quotient × r)
-# (old_s, s) := (s, old_s − quotient × s)
-# (old_t, t) := (t, old_t − quotient × t)
-#
-# output
-# "Bézout coefficients:", (old_s, old_t)
-# output
-# "greatest common divisor:", old_r
-# output
-# "quotients by the gcd:", (t, s)
-
-
 def extended_eculidean_gcd(a,b):
     a0 = a
     b0 = b
